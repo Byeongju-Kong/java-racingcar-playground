da6f1d1673bd661030b00922b4b5b26100f95840
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-12 16:14:11 +0900

    feat : CarName 테스트 및 구현

---
+++ src/main/java/model/CarName.java
@@ -0,0 +1,21 @@
+package model;
+
+public class CarName {
+    private static final int CAR_NAME_LENGTH_BOUNDARY = 5;
+    private final String name;
+
+    public CarName(String carName) {
+        validateCarName(carName);
+        this.name = carName;
+    }
+
+    private void validateCarName(String carName) {
+        if (carName.length() > CAR_NAME_LENGTH_BOUNDARY) {
+            throw new IllegalArgumentException("차 이름은 5글자보다 길 수 없습니다");
+        }
+    }
+
+    public String getCarName() {
+        return name;
+    }
+}
---
+++ src/test/java/model/CarNameTest.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarNameTest {
+    @ParameterizedTest
+    @DisplayName("5자를 넘는 문자열로 CarName을 생성하면 오류를 발생시킨다")
+    @ValueSource(strings = {"Carnival", "Sonata"})
+    void create_ExceptionByLongerNameThanFive(String carName) {
+        assertThatThrownBy(() -> new CarName(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차 이름은 5글자보다 길 수 없습니다");
+    }
+
+    @ParameterizedTest
+    @DisplayName("getCarName()으로 차 이름을 반환한다.")
+    @CsvSource({"Audi, Audi", "BMW, BMW"})
+    void getCarName(String givenCarName, String expect) {
+        CarName carName = new CarName(givenCarName);
+        String actual = carName.getCarName();
+        assertThat(actual).isEqualTo(expect);
+    }
+}
