12fa450bc203467e38f9664b0ab7f5c4439c5b3f
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-13 16:45:18 +0900

    refactor : generateRandomMovements 반환 타입 수정
    int[] -> boolean[]

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+T
+$src/main/java/model/car/CarName.java,d/1/d1918650711159f6235f61779fbd422411fcef34
+X
+(src/test/java/model/car/CarNameTest.java,3/8/38250af57083a958a6aef608f3b312baf14b1da3
+Y
+)src/test/java/model/car/DistanceTest.java,4/1/41ac53627c4498151b1868b578afe1d12e1fdf2a
--- src/main/java/util/RandomMovement.java
+++ src/main/java/util/RandomMovement.java
@@ -5,19 +5,18 @@ import java.util.stream.IntStream;
 
 public class RandomMovement {
     private static final Random random = new Random();
-    private static final int MOVEMENT_TIMES_LIMITS = 2;
     private static final int START_ENTRY = 0;
     private static final int NUMBER_OF_CARS = 3;
 
     private RandomMovement() {
     }
 
-    private static int generateRandomMovement() {
-        return random.nextInt(MOVEMENT_TIMES_LIMITS);
+    private static boolean generateRandomMovement() {
+        return random.nextBoolean();
     }
 
-    public static int[] generateRandomMovements() {
-        int[] randomMovements = new int[3];
+    public static boolean[] generateRandomMovements() {
+        boolean[] randomMovements = new boolean[3];
         IntStream.range(START_ENTRY, NUMBER_OF_CARS)
                 .forEach(index -> randomMovements[index] = generateRandomMovement());
         return randomMovements;
