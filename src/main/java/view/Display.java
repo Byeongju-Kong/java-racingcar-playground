06db205e3eced7b435043f32d4c3408b728064aa
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-14 21:52:44 +0900

    refactor : Display의 메서드에 static 키워드 추가

--- src/main/java/view/Display.java
+++ src/main/java/view/Display.java
@@ -4,11 +4,11 @@ public class Display {
     private Display(){
     }
 
-    public void alertInputCarNames() {
+    public static void alertInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
-    public void alertInputRacingRound() {
+    public static void alertInputRacingRound() {
         System.out.println("시도할 횟수는 몇회인가요?");
     }
 }
