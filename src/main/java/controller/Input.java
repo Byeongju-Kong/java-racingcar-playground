064cc17226553a23ef6c1cf658c2ac242118a1b0
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-14 21:42:13 +0900

    feat : Input 구현

---
+++ src/main/java/controller/Input.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.util.Scanner;
+
+public class Input {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private Input() {
+    }
+
+    public static String[] inputCarNames() {
+        return SCANNER.nextLine().split(",");
+    }
+
+    public static int inputRacingRound() {
+        return Integer.parseInt(SCANNER.nextLine());
+    }
+}
