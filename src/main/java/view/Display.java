3035a8fa46a433d35e796ce4aa48ae371407651d
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-15 17:36:44 +0900

    refactor : showRoundResult 메서드 추가

--- src/main/java/view/Display.java
+++ src/main/java/view/Display.java
@@ -1,7 +1,16 @@
 package view;
 
+import model.car.Car;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Display {
-    private Display(){
+    private static final String DISTANCE_SYMBOL = "-";
+    private static final int NUMBER_OF_CARS = 3;
+    private static boolean firstAccess = true;
+
+    private Display() {
     }
 
     public static void alertInputCarNames() {
@@ -10,5 +19,26 @@ public class Display {
 
     public static void alertInputRacingRound() {
         System.out.println("시도할 횟수는 몇회인가요?");
+    }
+
+    public static void showRoundResult(Car[] cars) {
+        if (firstAccess) {
+            System.out.println("\n실행결과\n");
+            firstAccess = false;
+        }
+        IntStream.range(0, NUMBER_OF_CARS)
+                .forEach(index -> System.out.print(cars[index].getCarName() + " : " +
+                        DISTANCE_SYMBOL.repeat(cars[index].getDistance()) + "\n"));
+        System.out.println();
+    }
+
+    public static void showRacingResult(List<Car> winner) {
+        StringBuilder result = new StringBuilder();
+        String lastWords = "가 최종 우승했습니다.";
+        IntStream.range(0, winner.size())
+                .forEach(index -> result.append(winner.get(index).getCarName()).append(","));
+        result.deleteCharAt(result.length() - 1);
+        result.append(lastWords);
+        System.out.println(result);
     }
 }
