57bdfca9157115ef561b19097c4103aafb97decb
Byeongju-Kong <bjuuuu98@gmail.com>
2021-08-16 14:30:36 +0900

    refactor : generateRandomMovements 간략화

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,7 +3,5 @@
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 T
 $src/main/java/model/car/CarName.java,d/1/d1918650711159f6235f61779fbd422411fcef34
-V
-&src/main/java/util/RandomMovement.java,0/5/055ad572f5e6eabca5f91fa87057a3037804cab6
 S
 #src/main/java/controller/Input.java,0/b/0ba450bc619d5beb74f886d622ea5a86d7af9f7a
--- src/main/java/util/RandomMovement.java
+++ src/main/java/util/RandomMovement.java
@@ -1,24 +1,14 @@
 package util;
 
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class RandomMovement {
     private static final Random random = new Random();
-    private static final int START_ENTRY = 0;
-    private static final int NUMBER_OF_CARS = 3;
 
     private RandomMovement() {
     }
 
-    private static boolean generateRandomMovement() {
-        return random.nextBoolean();
-    }
-
     public static boolean[] generateRandomMovements() {
-        boolean[] randomMovements = new boolean[3];
-        IntStream.range(START_ENTRY, NUMBER_OF_CARS)
-                .forEach(index -> randomMovements[index] = generateRandomMovement());
-        return randomMovements;
+        return new boolean[]{random.nextBoolean(), random.nextBoolean(), random.nextBoolean()};
     }
 }
