53677262d2f8fc370572480f4f88601981491dce
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-01 15:29:35 +0900

    feat : 라운드 결과를 출력하기 위한 RoundDTO 생성 및 테스트

---
+++ src/main/java/model/RoundDTO.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.List;
+
+public class RoundDTO {
+    private final List<String> carNames;
+    private final List<Integer> distances;
+
+    public RoundDTO(List<String> carNames, List<Integer> distances) {
+        this.carNames = carNames;
+        this.distances = distances;
+    }
+
+    public List<String> getCarNames() {
+        return carNames;
+    }
+
+    public List<Integer> getDistances() {
+        return distances;
+    }
+}
