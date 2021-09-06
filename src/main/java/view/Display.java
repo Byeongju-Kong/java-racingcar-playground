b570fa0698c66367303da62091bec3c042bcae9a
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-06 15:56:36 +0900

    refactor : distanceSymbol과 delimiter 지역변수 private static final 필드로 수정

--- src/main/java/view/Display.java
+++ src/main/java/view/Display.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Display implements Displayable{
+    private static final String DISTANCE_SYMBOL = "-";
+    private static final String DELIMITER = ",";
+
     public void alertInputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
@@ -20,20 +23,18 @@ public class Display implements Displayable{
     }
 
     public void showRoundResult(RoundDTO roundResult) {
-        String distanceSymbol = "-";
         List<String> carNames = roundResult.getCarNames();
         List<Integer> distances = roundResult.getDistances();
         IntStream.range(0, carNames.size())
-                .forEach(index -> System.out.println(carNames.get(index) + " : " + distanceSymbol.repeat(distances.get(index))));
+                .forEach(index -> System.out.println(carNames.get(index) + " : " + DISTANCE_SYMBOL.repeat(distances.get(index))));
     }
 
     public void showFinalResult(List<Car> winner) {
-        String delimiter = ",";
         StringBuilder result = new StringBuilder();
         String lastWords = "가 최종 우승했습니다.";
         IntStream.range(0, winner.size())
-                .forEach(index -> result.append(winner.get(index).getCarName()).append(delimiter));
-        result.deleteCharAt(result.lastIndexOf(delimiter));
+                .forEach(index -> result.append(winner.get(index).getCarName()).append(DELIMITER));
+        result.deleteCharAt(result.lastIndexOf(DELIMITER));
         System.out.print(result.append(lastWords));
     }
 }
