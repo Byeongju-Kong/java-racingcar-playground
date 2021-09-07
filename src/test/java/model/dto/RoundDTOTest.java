6af531e54d453840b66296b7172388cdbb36e990
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-07 13:06:27 +0900

    refactor : Convention에 맞게 공백 추가

--- src/test/java/model/dto/RoundDTOTest.java
+++ src/test/java/model/dto/RoundDTOTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RoundDTOTest {
     private RoundDTO roundDTO;
+
     @BeforeEach
     void setUp() {
         List<String> carNames = Arrays.asList("Audi", "Benz", "BMW");
