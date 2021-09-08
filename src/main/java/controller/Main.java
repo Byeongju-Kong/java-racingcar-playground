b02ef5efcd386b0743d10e6cad819c1cf5068f90
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-08 17:47:30 +0900

    refactor : RacingGameController 생성자로 Movement, Display 주입 받도록 수정

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,8 +1,19 @@
 package controller;
 
+import model.movement.Movement;
+import model.movement.RandomMovement;
+import view.Display;
+import view.RacingDisplay;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        RacingGameController racingGameController = new RacingGameController();
+        Scanner scanner = new Scanner(System.in);
+        Display display = new RacingDisplay(scanner);
+        Movement movement = new RandomMovement();
+        RacingGameController racingGameController = new RacingGameController(display, movement);
         racingGameController.run();
+        scanner.close();
     }
 }
--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,34 +1,29 @@
 package controller;
 
 import model.Racing;
+import model.car.Car;
+import model.movement.Movement;
 import view.Display;
-import view.Displayable;
 
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static controller.Input.inputCarNames;
-import static controller.Input.inputRacingRound;
 
 public class RacingGameController {
     private final Racing racing;
     private final Integer numberOfCars;
-    private final Displayable display = new Display();
+    private final Display display;
+    private final Movement movement;
     private static final int ENDING_ROUND = 0;
 
-    public RacingGameController() {
-        display.alertInputCarNames();
-        String[] carNames = inputCarNames();
+    public RacingGameController(Display display, Movement movement) {
+        this.display = display;
+        this.movement = movement;
+        String[] carNames = display.inputCarNames();
         numberOfCars = carNames.length;
         racing = new Racing(carNames);
     }
 
     public void run() {
-        display.alertInputRacingRound();
-        int racingRound = inputRacingRound();
-        display.showStart();
+        int racingRound = display.inputRacingRound();
         while (racingRound-- > ENDING_ROUND) {
             race();
         }
@@ -36,14 +31,7 @@ public class RacingGameController {
     }
 
     private void race() {
-        racing.race(generateRandomMovements(numberOfCars));
-        display.showRoundResult(racing.getRoundResult());
-    }
-
-    private List<Boolean> generateRandomMovements(final int numberOfCars) {
-        Random random = new Random();
-        return Stream.generate(random::nextBoolean)
-                .limit(numberOfCars)
-                .collect(Collectors.toList());
+        List<Car> racedCars = racing.race(movement.generateRandomMovements(numberOfCars));
+        display.showRoundResult(racedCars);
     }
 }
