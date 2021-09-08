27f6a79ac11f93135c6ab10f43e14c45c5efb084
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-08 18:09:00 +0900

    refactor : ' '를 private static final 변수로 추출

--- src/main/java/model/vo/CarName.java
+++ src/main/java/model/vo/CarName.java
@@ -2,6 +2,7 @@ package model.vo;
 
 public class CarName {
     private static final Integer CAR_NAME_LENGTH_BOUNDARY = 5;
+    private static final char EMPTY = ' ';
     private final String name;
 
     public CarName(final String carName) {
@@ -13,7 +14,7 @@ public class CarName {
         if (carName.length() > CAR_NAME_LENGTH_BOUNDARY) {
             throw new IllegalArgumentException("차 이름은 5글자보다 길 수 없습니다");
         }
-        if (carName.charAt(0) == ' ') {
+        if (carName.charAt(0) == EMPTY) {
             throw new IllegalArgumentException("차 이름은 공백으로 시작할 수 없고, 차 이름을 공백으로 생성할 수 없습니다.");
         }
     }
