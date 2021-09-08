b371cb3c583aa3894d45098a8c98f65a7d34b4a3
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-08 17:38:47 +0900

    refactor : view 수정
    인터페이스로 분리하고 view에서 입출력을 모두 담당

--- src/main/java/view/Display.java
+++ src/main/java/view/Display.java
@@ -1,44 +1,15 @@
 package view;
 
-import model.dto.RoundDTO;
 import model.car.Car;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
-public class Display implements Displayable {
-    private static final String DISTANCE_SYMBOL = "-";
-    private static final String DELIMITER = ",";
+public interface Display {
+    String[] inputCarNames();
 
-    @Override
-    public void alertInputCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-    }
+    int inputRacingRound();
 
-    @Override
-    public void alertInputRacingRound() {
-        System.out.println("시도할 횟수는 몇회인가요?");
-    }
+    void showRoundResult(List<Car> cars);
 
-    @Override
-    public void showStart() {
-        System.out.println("\n실행결과\n");
-    }
-
-    @Override
-    public void showRoundResult(RoundDTO roundResult) {
-        IntStream.range(0, roundResult.getCarNames().size())
-                .forEach(index -> System.out.println(roundResult.getCarNames().get(index) + " : "
-                        + DISTANCE_SYMBOL.repeat(roundResult.getDistances().get(index))));
-    }
-
-    @Override
-    public void showFinalResult(List<Car> winner) {
-        StringBuilder result = new StringBuilder();
-        String lastWords = "가 최종 우승했습니다.";
-        IntStream.range(0, winner.size())
-                .forEach(index -> result.append(winner.get(index).getCarName()).append(DELIMITER));
-        result.deleteCharAt(result.lastIndexOf(DELIMITER));
-        System.out.print(result.append(lastWords));
-    }
+    void showFinalResult(List<Car> winners);
 }
---
+++ src/main/java/view/RacingDisplay.java
@@ -0,0 +1,46 @@
+package view;
+
+import model.car.Car;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class RacingDisplay implements Display {
+    private final Scanner scanner;
+    private static final String DISTANCE_SYMBOL = "-";
+    private static final String DELIMITER = ",";
+
+    public RacingDisplay(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public String[] inputCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        return scanner.nextLine().split(",");
+    }
+
+    @Override
+    public int inputRacingRound() {
+        System.out.print("시도할 횟수는 몇회인가요? ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    @Override
+    public void showRoundResult(List<Car> cars) {
+        IntStream.range(0, cars.size())
+                .forEach(index -> System.out.println(cars.get(index).getCarName() + " : "
+                        + DISTANCE_SYMBOL.repeat(cars.get(index).getDistance())));
+    }
+
+    @Override
+    public void showFinalResult(List<Car> winners) {
+        StringBuilder result = new StringBuilder();
+        String lastWords = "가 최종 우승했습니다.";
+        IntStream.range(0, winners.size())
+                .forEach(index -> result.append(winners.get(index).getCarName()).append(DELIMITER));
+        result.deleteCharAt(result.lastIndexOf(DELIMITER));
+        System.out.print(result.append(lastWords));
+    }
+}
