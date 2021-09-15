3ec911a777f92edd3135ddff3e5459714687a76a
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-15 15:05:08 +0900

    refactor : isShorter 메서드 추가

--- src/main/java/model/vo/Distance.java
+++ src/main/java/model/vo/Distance.java
@@ -16,6 +16,10 @@ public class Distance {
         return movementTimes;
     }
 
+    public boolean isShorter(Distance another) {
+        return movementTimes <= another.movementTimes;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
