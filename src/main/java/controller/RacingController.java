06c71b66e0da41c833f862932f30b92932f1e491
Byeongju-Kong <bjuuuu98@gmail.com>
2021-10-12 15:49:41 +0900

    refactor : RacingRound 적용 및 findWinners 반환타입 변경 적용

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,38 +1,41 @@
 package controller;
 
 import model.Racing;
+import model.RacingRound;
 import model.Referee;
 import model.movement.MovementStrategy;
 import model.movement.RandomMovement;
 import view.Display;
 import view.RacingDisplay;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingController {
-    private static final int END = 0;
     private final Racing racing;
     private final Display display;
     private final MovementStrategy movement;
+    private final RacingRound racingRound;
 
     public RacingController(final Display display, final MovementStrategy movement) {
         this.display = display;
         this.movement = movement;
         racing = new Racing(display.inputCarNames());
+        racingRound = new RacingRound(display.inputRacingRound());
     }
 
     public void run() {
-        int racingRound = display.inputRacingRound();
-        while (racingRound-- > END) {
+        while (!racingRound.isOver()) {
             race();
         }
-        String[] winners = new Referee().findWinners(racing.getRacedResult());
+        List<String> winners = new Referee().findWinners(racing.getRacedResult());
         display.showWinners(winners);
     }
 
     private void race() {
         racing.race(movement);
         display.showRoundResult(racing.getRacedResult());
+        racingRound.decreaseLeftRound();
     }
 
     public static void main(String[] args) {
